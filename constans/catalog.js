20c09363507f5b6b00d9363fec914a692d7db6a2
Vadim <shadrinv686@gmail.com>
2021-10-21 21:59:19 +0600

    Filled catalog

---
+++ constans/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: 'elem1',
+        name: 'Nokia',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 15000,
+    },
+    {
+        id: 'elem2',
+        name: 'Samsung',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 20000,
+    },
+    {
+        id: 'elem3',
+        name: 'Motorola',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 1000,
+    },
+    {
+        id: 'elem1',
+        name: 'SonyEricsson',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 4000,
+    },
+    {
+        id: 'elem1',
+        name: 'Xiaomi',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 14000,
+    },
+    {
+        id: 'elem1',
+        name: 'Honor',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 10000,
+    },
+    {
+        id: 'elem1',
+        name: 'BQ',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 4000,
+    },
+    {
+        id: 'elem1',
+        name: 'Alcatel',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 5500,
+    },
+    {
+        id: 'elem1',
+        name: 'Huawei',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 6000,
+    },
+    {
+        id: 'elem1',
+        name: 'IPhone',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/12/46/iphone-160307_960_720.png',
+        price: 80000,
+    },
+];
