a22aa2b4e293bb81ced3b6e5f97ac2c2f0a277e0
Vadim <shadrinv686@gmail.com>
2021-10-26 23:02:30 +0600

    Add style button element

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -7,7 +7,7 @@ class Products {
             <li class="products-element">
                 <span class="products-element_name">${name}</span>
                 <img class="products-element_img" src="${img}" />
-                <span class="products-element_price">${price}</span>
+                <span class="products-element_price">💵 ${price}</span>
                 <button class="products-element_button">Add to shopping</button>
             </li>
             `;
