46694f1698a6b20d33d82d52dd9ad45e4da9db9c
Vadim <shadrinv686@gmail.com>
2021-10-28 22:44:26 +0600

    Add method putProducts

--- utils/LocalStorageUtil.js
+++ utils/LocalStorageUtil.js
@@ -1,18 +1,20 @@
 class LocalStorageUtil {
     constructor() {
-        this.keyName = 'products';    
+        this.keyName = 'products';
     }
 
     getProducts() {
         const productsLocalStorage = localStorage.getItem(this.keyName);
-        if(productsLocalStorage !== null) {
+        if (productsLocalStorage !== null) {
             return JSON.parse(productsLocalStorage);
         }
         return [];
     }
 
     putProducts(id) {
-
+        let products = this.getProducts();
+        products.push(id);
+        localStorage.setItem(this.keyName, JSON.stringify(products));
     }
 }
 const localStorageUtil = new LocalStorageUtil();
