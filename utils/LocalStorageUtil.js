734f421bc6bcb9a46060cccaec5e7df37fb2c280
Vadim <shadrinv686@gmail.com>
2021-10-28 23:03:57 +0600

    Add check for putProduckts

--- utils/LocalStorageUtil.js
+++ utils/LocalStorageUtil.js
@@ -13,11 +13,20 @@ class LocalStorageUtil {
 
     putProducts(id) {
         let products = this.getProducts();
+        let pushProduct = false;
+        const index = products.indexOf(id);
+
+        if (index === -1) {
         products.push(id);
+        pushProduct = true;
+        } else {
+            products.splice(index, 1)
+        }
         localStorage.setItem(this.keyName, JSON.stringify(products));
+
+        return { pushProduct, products }
     }
 }
 const localStorageUtil = new LocalStorageUtil();
 
 const a = localStorageUtil.getProducts();
-console.log(a);
