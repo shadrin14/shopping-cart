caa5f2dffa68a60070556e08b8d2f5179f5e85b8
Vadim <shadrinv686@gmail.com>
2021-11-22 23:40:36 +0600

    Rendering headerPage

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,10 +3,15 @@ class Header {
     render() {
         const html = `
             <div class="header-container">
-                <div class="header-counter"></div>
+                <div class="header-counter">
+                🛒 10
+                </div>
             </div>
         `;
+
+        ROOT_HEADER.innerHTML = html;
     }
 }
 
 const headerPage = new Header();
+headerPage.render();
