d09f721eb69571e27c7d9a015cd595b6a9e221f5
Vadim <shadrinv686@gmail.com>
2021-11-22 23:29:55 +0600

    Add class Header

---
+++ components/Header/Header.js
@@ -0,0 +1,12 @@
+class Header {
+    
+    render() {
+        const html = `
+            <div class="header-container">
+                <div class="header-counter"></div>
+            </div>
+        `;
+    }
+}
+
+const headerPage = new Header();
